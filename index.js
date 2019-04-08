280acfe8f119a110cc477df25847d55401e7187b
Molly Nelson <mollykathrynnelson@gmail.com>
2019-04-08 03:04:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ function scuberGreetingForFeet(feet){
 function  ternaryCheckCity(city) {
     return (city === "NYC" ? "Ok, sounds good." : "No go.");
 }
+
+function switchOnCharmFromTip(tip){
+    switch (tip){
+      case "generous":
+        "Thank you so much."
+      break;
+    }
+}
