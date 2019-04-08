b02972c7e26ce1f0b7504cda1388c62835983d0a
Molly Nelson <mollykathrynnelson@gmail.com>
2019-04-08 02:51:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Write your code in this file!
 
 function scuberGreetingForFeet(feet){
-    if (feet >== 400) {
-      return 'This one is on me!';      
+    if (feet <= 400) {
+      return 'This one is on me!';
     }
 
 
