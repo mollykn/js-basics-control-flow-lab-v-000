46de23ac116fc1b6eef394b7fa34b999efdcdc3c
Molly Nelson <mollykathrynnelson@gmail.com>
2019-04-08 03:05:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function  ternaryCheckCity(city) {
 function switchOnCharmFromTip(tip){
     switch (tip){
       case "generous":
-        "Thank you so much."
-      break;
+        return "Thank you so much.";
+        
     }
 }
